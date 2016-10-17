e7465e74f36b5632e927da1a3831e487d7fc3357
LunaMar <letera.lmc@gmail.com>
2016-10-16 22:32:09 -0300

    ejercicios

---
+++ script.js
@@ -0,0 +1,49 @@
+//numero menor //
+
+function menor(num1,num2,num3){
+	var resultado =Math.min(num1,num2,num3);
+	return resultado;
+}
+
+//Numero par o menor//
+
+function numeroPar(num){
+
+	if (num%2===0){
+		alert(numero+= "Es par")
+	} else {
+		alert(numero+= "Es impar")
+	}
+}
+
+//ordena los Numeros//
+
+function ordenar(n1,n2,n3){
+	var numero= n1,n2,n3
+	var ordenado =numero.sor()
+ return  a-b ;
+
+// mayuscula o minuscula //
+
+function mayuscula(cadena){
+	
+	var resultado="el texto "+ cadena +" ";
+	if(cadena===cadena.toUpperCase()){
+		resultado+="son solo mayuscula"
+	}else if(cadena ===cadena.toLowerCase()){
+		resultado+="solo minusculas";
+	}else{
+		resultado +="es ambas";
+	}
+	return resultado;
+}
+//cuadrado de la suma de dos numeros//
+
+function suma (a+b){
+	
+	var resultado=(a+b);
+	var cuadrado= resultado*resultado;
+	return cuadrado;
+}
+
+
